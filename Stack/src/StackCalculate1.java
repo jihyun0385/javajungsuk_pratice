b3a7dcd427ab72fed294d6c50918d7ccfc24221d
jihyun0385 <133867376+jihyun0385@users.noreply.github.com>
2023-07-18 00:18:42 +0900

    Update StackCalculate1.java

--- Stack/src/StackCalculate1.java
+++ Stack/src/StackCalculate1.java
@@ -3,38 +3,31 @@ import java.util.*;
 public class StackCalculate1 {
 
 	public static void main(String[] args) {
-//		Stack st = new Stack();
-		ArrayList list1 = new ArrayList();
+		Stack<String> st = new Stack<>();  // 숫자 담기 
+		Stack<String> st2 = new Stack<>(); // 연산자 담기 
 		String num = "";
 
 		Scanner scan = new Scanner(System.in);
 
 		System.out.print("계산식을 입력하세요 (+,-,/,*)");
 		String input = scan.nextLine();
+		
 
 		for (int i = 0; i < input.length(); i++) {
 			char ch = input.charAt(i);
-			if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '(' || ch == ')') {
-				list1.add(num);
-				num = ""; // 변수 초기화
-				list1.add(ch + ""); // 연산자를 문자열로 저장
+			if (ch == '*' || ch == '/' || ch == '+' || ch == '-'|| ch == '(' || ch == ')') {
+				if (!num.isEmpty()) {
+					st.push(num); // 숫자를 st에 넣기 
+					num = ""; 
+				}
+				st2.push(ch + ""); // 연산자 넣어주기 
 			} else {
-				num = num + ch;
+				num = num + ch; 
 			}
 		}
 
-		list1.add(num); // 마지막 숫
-		list1.remove("");
-
-		System.out.println(list1);
-
+		System.out.println(st);
+		System.out.println(st2);
 		}
-
 	}
-//	while(!st.empty())
-//
-//	{
-//		System.out.println(st.pop());
-//	}
-//
-//}}
+
